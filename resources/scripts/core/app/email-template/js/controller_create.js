e1055272ac9ca8d29588b0df07a02386d40363b1
tuandev <tuan@relotalent.com>
2023-08-17 15:42:19 +0700

    28

---
+++ resources/scripts/core/app/email-template/js/controller_create.js
@@ -0,0 +1,30 @@
+/**
+ * Created on dd/mm/yyyy.
+ */
+(function () {
+    'use strict';
+
+    App.controller('EmailTemplateCreateController',  ['$scope', '$http', '$stateParams', '$state', '$timeout', 'ngDialog', 'urlBase', 'AppSystem', 'WaitingService', 'AppEmailTemplateService', 'emailTemplate',
+        function ($scope, $http, $stateParams, $state, $timeout, ngDialog, urlBase, AppSystem, WaitingService, AppEmailTemplateService, emailTemplate) {
+
+            $scope.emailTemplate = {
+                name: null, description: null
+            };
+
+            $scope.saveFn = function () {
+                WaitingService.begin();
+                AppEmailTemplateService.create($scope.emailTemplate).then(function (res) {
+                    if (res.success) {
+                        $scope.closeThisDialog(res.data)
+                    } else {
+                        WaitingService.expire(res);
+                    }
+                    WaitingService.end();
+                }, function (error) {
+                    WaitingService.end();
+                    WaitingService.expire(error);
+                });
+            }; // End save function
+        }
+    ]);
+})();
